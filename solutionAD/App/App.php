56664eca0378f0f08a6dffd02d3b47a86a56d8b9
Armands <armandsdulbinskis@gmail.com>
2020-01-10 15:05:23 +0200

    small fix

--- solutionAD/App/App.php
+++ solutionAD/App/App.php
@@ -15,29 +15,26 @@ class App
     public function getResult()
     {
 
-
         $result = '';
         $number3 = $this->number;
         $number3 %= 3;
         if ($number3 == 0)
             $result .= 'Foo';
 
-
-            $number5 = $this->number;
-            $number5 %= 5;
-            if ($number5 == 0) {
-                if ($result == 'Foo') {
-                    $result .= ', ';
-                }
-                $result .= 'Bar';
+        $number5 = $this->number;
+        $number5 %= 5;
+        if ($number5 == 0) {
+            if ($result == 'Foo') {
+                $result .= ', ';
             }
+            $result .= 'Bar';
+        }
 
-            if($result==''){
-                return $this->number;
-            }else{
-                return $result;
-            }
-
+        if ($result == '') {
+            return $this->number;
+        } else {
+            return $result;
+        }
 
     }
 }

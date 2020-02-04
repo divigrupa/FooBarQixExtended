ddfb7090c553118f75ad81372b68149e868afdbc
Armands <armandsdulbinskis@gmail.com>
2020-02-04 14:11:33 +0200

    added example

--- solutionAD/tests/unit/FooBarQixCest.php
+++ solutionAD/tests/unit/FooBarQixCest.php
@@ -3,74 +3,135 @@ include dirname(__DIR__, 2) . '/App/App.php';
 
 class FooBarQixCest
 {
-    public function givenNumberDividesWith3ReturnFoo(UnitTester $I)
+    /**
+     * @example [3]
+     * @example [6]
+     * @example [19564434]
+     */
+    public function givenNumberDividesWith3ReturnFoo(UnitTester $I, \Codeception\Example $example)
     {
         $app = new App(3);
         $result = $app->getResult();
         $I->assertEquals('Foo', $result);
     }
 
-    public function givenNumberDividesWith5ReturnBar(UnitTester $I)
+
+    /**
+     * @example [5]
+     * @example [10]
+     * @example [5555555555]
+     */
+    public function givenNumberDividesWith5ReturnBar(UnitTester $I, \Codeception\Example $example)
     {
-        $app = new App(5);
+        $app = new App($example[0]);
         $result = $app->getResult();
         $I->assertEquals('Bar', $result);
     }
 
-    public function givenNumberDividesWith7ReturnQix(UnitTester $I)
+
+    /**
+     * @example [7]
+     * @example [14]
+     * @example [41274247]
+     */
+    public function givenNumberDividesWith7ReturnQix(UnitTester $I, \Codeception\Example $example)
     {
-        $app = new App(7);
+        $app = new App($example[0]);
         $result = $app->getResult();
         $I->assertEquals('Qix', $result);
     }
 
-    public function givenNumberDividesWith5And3ReturnFooBar(UnitTester $I)
+
+    /**
+     * @example [15]
+     * @example [30]
+     * @example [84482205]
+     */
+    public function givenNumberDividesWith5And3ReturnFooBar(UnitTester $I, \Codeception\Example $example)
     {
-        $app = new App(15);
+        $app = new App($example[0]);
         $result = $app->getResult();
         $I->assertEquals('Foo, Bar', $result);
     }
 
-    public function givenNumberDividesWith5And3And7ReturnFooBarQix(UnitTester $I)
-    {
-        $app = new App(105);
-        $result = $app->getResult();
-        $I->assertEquals('Foo, Bar, Qix', $result);
-    }
 
-    public function givenNumberDividesWith3And7ReturnFooBar(UnitTester $I)
+    /**
+     * @example [21]
+     * @example [42]
+     * @example [40456836]
+     */
+    public function givenNumberDividesWith3And7ReturnFooBar(UnitTester $I, \Codeception\Example $example)
     {
-        $app = new App(21);
+        $app = new App($example[0]);
         $result = $app->getResult();
         $I->assertEquals('Foo, Qix', $result);
     }
 
-    public function givenNumberDividesWith5And7ReturnFooBar(UnitTester $I)
+
+    /**
+     * @example [35]
+     * @example [70]
+     * @example [342493760]
+     */
+    public function givenNumberDividesWith5And7ReturnFooBar(UnitTester $I, \Codeception\Example $example)
     {
-        $app = new App(35);
+        $app = new App($example[0]);
         $result = $app->getResult();
         $I->assertEquals('Bar, Qix', $result);
     }
 
-    public function givenNegativeNumberThrowException(UnitTester $I)
-    {
-        $I->expectThrowable(Exception::class, function () {
-            (new App(-35))->getResult();
-        });
-    }
 
-    public function givenNumberIsNotIntegerThrowException(UnitTester $I)
+    /**
+     * @example [105]
+     * @example [210]
+     * @example [53438490]
+     */
+    public function givenNumberDividesWith5And3And7ReturnFooBarQix(UnitTester $I, \Codeception\Example $example)
     {
-        $I->expectThrowable(Exception::class, function () {
-            (new App('7'))->getResult();
-        });
-    }
-
-    public function returnTheGivenNumberAsAStringIfThereIsNoTransformationToDo(UnitTester $I)
-    {
-        $app = new App(92);
+        $app = new App($example[0]);
         $result = $app->getResult();
-        $given =$app->number;
+        $I->assertEquals('Foo, Bar, Qix', $result);
+    }
+
+
+    /**
+     * @example [-105]
+     * @example [-4]
+     * @example [-123456789]
+     */
+    public function givenNegativeNumberThrowException(UnitTester $I, \Codeception\Example $example)
+    {
+        $app = new App($example[0]);
+        $I->expectThrowable(Exception::class, function () use ($app) {
+            $app->getResult();
+        });
+    }
+
+    /**
+     * @example [true]
+     * @example ["7"]
+     * @example [5.5]
+     * @example [null]
+     */
+    public function givenNumberIsNotIntegerThrowException(UnitTester $I, \Codeception\Example $example)
+    {
+        $app = new App($example[0]);
+        $I->expectThrowable(Exception::class, function () use ($app) {
+            $app->getResult();
+        });
+    }
+
+
+    /**
+     * @example [1]
+     * @example [2]
+     * @example [4444444444]
+     */
+    public function returnTheGivenNumberAsAStringIfThereIsNoTransformationToDo(UnitTester $I, \Codeception\Example $example)
+    {
+        $app = new App($example[0]);
+        $result = $app->getResult();
+        $given = $app->number;
         $I->assertEquals($given, $result);
     }
 }

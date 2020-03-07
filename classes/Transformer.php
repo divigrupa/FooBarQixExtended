c4eefe442739700569922662e7c418049529866c
Xamresor <Romans.sereda@gmail.com>
2020-03-07 14:23:48 +0200

    Romans Sereda, done w3

--- classes/Transformer.php
+++ classes/Transformer.php
@@ -1,23 +1,63 @@
 <?php
 class Transformer
 {
-    /**
-     * @param int $number
-     * @return string
-     */
-	public static function divTransform(int $number): string
+	/**
+	 * @param int $number
+	 * @return string
+	 */
+	public static function fullTransform(int $number): string
+	{
+		$result = array_filter([
+			self::divTransform($number, false),
+			self::attachTransform($number, false)
+		]);
+		return !empty($result)
+			? implode(', ', $result)
+			: $number;
+	}
+
+	/**
+	 * @param int $number
+	 * @param bool $needNumber
+	 * @return string
+	 */
+	public static function divTransform(int $number, bool $needNumber = true): string
 	{
 		$result = [];
 		if ($number) {
 			if (!fmod($number,3)) $result[] = 'Foo';
 			if (!fmod($number,5)) $result[] = 'Bar';
-            if (!fmod($number,7)) $result[] = 'Qix';
+			if (!fmod($number,7)) $result[] = 'Qix';
 		}
 
 		if (!empty($result)) {
 			return implode(', ', $result);
 		}
-		return $number;
+		return $needNumber ? $number : '';
+	}
+
+	/**
+	 * @param int $number
+	 * @param bool $needNumber
+	 * @return string
+	 */
+	public static function attachTransform(int $number, bool $needNumber = true): string
+	{
+		$result = [];
+		if (!empty($number)) {
+			$charRow = (string) abs($number);
+			$charArray = str_split($charRow);
+
+			foreach ($charArray as $char) {
+				if (!fmod($char, 3)) $result[] = 'Foo';
+				if (!fmod($char, 5)) $result[] = 'Bar';
+				if (!fmod($char, 7)) $result[] = 'Qix';
+			}
+		}
+		if (!empty($result)) {
+			return implode(', ', $result);
+		}
+		return $needNumber ? $number : '';
 	}
 }
 
--- res/test.php
+++ res/test.php
@@ -4,6 +4,7 @@
  * It will take a number (positive integer) and provide :
  *    "Foo" if this number is multiple of 3
  *    "Bar" if this number is multiple of 5
+ *    "Qix" if this number is multiple of 7
  */
 $testCases = [
 	-15 => 'Foo, Bar',
@@ -32,4 +33,56 @@ foreach ($testCases as $number => $expected) {
     }
 }
 
+/**
+ * The new rules are : If the given number contains specific digit,
+ * we will apppend a word to the transformation in the order they appear in the number.
+ * So if a number contains :
+ *    3, then we append "Foo"
+ *    5, then we append "Bar"
+ *    7, then we append "Qix"
+ */
+$testCases = [
+    -1142881 => '-1142881',
+    -3314123 => 'Foo, Foo, Foo',
+    -2314 => 'Foo',
+    -1321 => 'Foo',
+    0 => '0',
+    153743 => 'Bar, Foo, Qix, Foo',
+    231415 => 'Foo, Bar',
+    3231 => 'Foo, Foo',
+];
+foreach ($testCases as $number => $expected) {
+    $actual = Transformer::attachTransform($number);
+    assert($actual === $expected);
+    if ($actual === $expected) {
+        d("Success {$number} => {$expected}<br>");
+    } else {
+        d(['number' => $number, 'expected' => $expected, 'actual' => $actual]);
+    }
+}
+
+/**
+ * Here, you have to add test for the new functionnality
+ * and add some to test numbers that triggers both functionnality
+ * in order to know if they work well together.
+ */
+$testCases = [
+    -1142881 => '-1142881',
+    -3314123 => 'Foo, Foo, Foo',
+    -2314 => 'Foo',
+    -1321 => 'Foo',
+    0 => '0',
+    153743 => 'Bar, Foo, Qix, Foo',
+    231415 => 'Bar, Foo, Bar',
+    3231 => 'Foo, Foo, Foo',
+];
+foreach ($testCases as $number => $expected) {
+    $actual = Transformer::fullTransform($number);
+    assert($actual === $expected);
+    if ($actual === $expected) {
+        d("Success {$number} => {$expected}<br>");
+    } else {
+        d(['number' => $number, 'expected' => $expected, 'actual' => $actual]);
+    }
+}
 

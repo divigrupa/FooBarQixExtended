0b130f5dbd40ab97d50cd8cd4709853fe30fc88f
Raitis <raioz@inbox.lv>
2020-06-19 21:37:04 +0300

    Step 1 - created implementation

--- RaitisOzols/Solution/Logic.php
+++ RaitisOzols/Solution/Logic.php
@@ -4,4 +4,33 @@ declare(strict_types=1);
 
 class Logic
 {
+    private $foo = 'Foo';
+    private $bar = 'Bar';
+    private $result;
+    
+    public function serviceFooBar(int $input)
+    {
+        if ($input <= 0)
+        {
+            throw new InvalidArgumentException(
+                'Only positive integers allowed!'
+            );
+        }
+        
+        $this->result = '';
+        
+        if ($input % 3 == 0) {
+            $this->result = $this->foo;
+        }
+        
+        if ($input % 5 == 0) {
+            $this->result .= $this->bar;
+        }
+        
+        if ($input % 3 != 0 && $input % 5 != 0) {
+           $this->result = strval($input);
+        }
+        
+        return $this->result;
+    }
 }

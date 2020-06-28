80424d23ea9f27e43654f70a428ed82ed18a051e
Aigars (Garisons) Rogozovs <admin@fury.lv>
2020-06-29 01:59:47 +0300

    Step 3 test

--- tests/FooBarTest.php
+++ tests/FooBarTest.php
@@ -80,4 +80,54 @@ class FooBarTest extends Unit
     {
         $this->assertEquals('string', gettype($this->fooBar->run(1)));
     }
+    
+    public function testNewRulesWithExludedFromRules()
+    {
+        $numbers = [
+            1, 2, 4, 6, 8, 9, 10, 11, 111, 999
+        ];
+
+        foreach ($numbers as $number) {
+            $this->assertEquals($number, $this->fooBar->newRules($number));
+        }
+    }
+
+    public function testNewRulesIfThreeReplacedWithFoo()
+    {
+        $this->assertEquals('Foo', $this->fooBar->newRules(3));
+    }
+
+    public function testNewRulesIfFiveReplacedWithBar()
+    {
+        $this->assertEquals('Bar', $this->fooBar->newRules(5));
+    }
+
+    public function testNewRulesIfSevenReplacedWithQix()
+    {
+        $this->assertEquals('Qix', $this->fooBar->newRules(7));
+    }
+
+    public function testNewRulesIfPlainCombinationsReturnCorrectly()
+    {
+        $this->assertEquals('FooBarQix', $this->fooBar->newRules(357));
+        $this->assertEquals('FooBar', $this->fooBar->newRules(35));
+        $this->assertEquals('FooQix', $this->fooBar->newRules(37));
+        $this->assertEquals('BarQix', $this->fooBar->newRules(57));
+
+        $this->assertEquals('QixBarFoo', $this->fooBar->newRules(753));
+        $this->assertEquals('QixBar', $this->fooBar->newRules(75));
+        $this->assertEquals('QixFoo', $this->fooBar->newRules(73));
+        $this->assertEquals('BarFoo', $this->fooBar->newRules(53));
+
+        $this->assertEquals('FooQixBar', $this->fooBar->newRules(375));
+    }
+
+    public function testNewRulesIfMixedCombinationsReturnCorrectly()
+    {
+        $this->assertEquals('FooBarQix', $this->fooBar->newRules(1357));
+        $this->assertEquals('FooBarQix', $this->fooBar->newRules(132547999));
+        $this->assertEquals('FooBar', $this->fooBar->newRules(235));
+        $this->assertEquals('FooQix', $this->fooBar->newRules(437));
+        $this->assertEquals('BarQix', $this->fooBar->newRules(657));
+    }
 }

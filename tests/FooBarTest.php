76b93533423ed1276470a6c29c0a4dcdb7c173eb
Aigars (Garisons) Rogozovs <admin@fury.lv>
2020-06-28 23:37:06 +0300

    FooBar class test

---
+++ tests/FooBarTest.php
@@ -0,0 +1,64 @@
+<?php
+
+use \Codeception\Test\Unit;
+use \FooBar\FooBar;
+
+// @codingStandardsIgnoreLine
+class FooBarTest extends Unit
+{
+    private $fooBar = null;
+
+    // @codingStandardsIgnoreLine
+    protected function _before()
+    {
+        $this->fooBar = new FooBar;
+    }
+
+    public function testCheckFirstFifteenNumbersStatically()
+    {
+        $exceptResults = [
+            '',
+            '1',
+            '2',
+            'Foo',
+            '4',
+            'Bar',
+            'Foo',
+            '7',
+            '8',
+            'Foo',
+            'Bar',
+            '11',
+            'Foo',
+            '13',
+            '14',
+            'FooBar'
+        ];
+
+        for ($i = 1; $i <= 15; $i++) {
+            $this->assertEquals($exceptResults[$i], $this->fooBar->run($i));
+        }
+    }
+
+    public function testCheckNegativeInteger()
+    {
+        $this->expectException(Exception::class);
+        $this->fooBar->run(-1);
+    }
+
+    public function testCheckIfThreeReturnFoo()
+    {
+        $this->assertEquals('Foo', $this->fooBar->run(3));
+    }
+
+    public function testCheckIfFiveReturnBar()
+    {
+        $this->assertEquals('Bar', $this->fooBar->run(5));
+    }
+
+    
+    public function testCheckIfOneReturnString()
+    {
+        $this->assertEquals('string', gettype($this->fooBar->run(1)));
+    }
+}

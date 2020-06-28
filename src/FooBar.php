5df11b85be159b2986021a4a5751ead5e12b3dde
Aigars (Garisons) Rogozovs <admin@fury.lv>
2020-06-29 02:00:15 +0300

    Step 3 FooBar class with new rules

--- src/FooBar.php
+++ src/FooBar.php
@@ -36,4 +36,31 @@ class FooBar
 
         return $result;
     }
+
+    public function newRules($number = 1): string
+    {
+        if ((is_int($number) || ctype_digit($number))
+            && (int) $number < 1
+        ) {
+            throw new \Exception('Not positive integer');
+        }
+
+        $result = '';
+
+        $multipliers = [
+            3 => 'Foo',
+            5 => 'Bar',
+            7 => 'Qix',
+        ];
+
+        $cleanedNumber = preg_replace("/[^357]/", '', $number);
+
+        foreach ($multipliers as $key => $name) {
+            $cleanedNumber = preg_replace("/$key/", $name, $cleanedNumber);
+        }
+
+        $result = strlen($cleanedNumber) < 1 ? $number : $cleanedNumber;
+
+        return $result;
+    }
 }

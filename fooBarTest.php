5b892e30043e218f8f30feed8834dd740ef84405
RihardsMacijauskis <71095635+RihardsMacijauskis@users.noreply.github.com>
2020-09-11 10:56:19 +0300

    Add files via upload

---
+++ fooBarTest.php
@@ -0,0 +1,36 @@
+<?php 
+include(dirname(__FILE__)."/../fooBar.php");
+use PHPUnit\Framework\TestCase;
+
+    class fooBarTest extends TestCase
+    {
+    	public function testFloat(){
+    		$number = new fooBar();
+    		$this -> assertIsNotInt ($number->checkFooBar(3.2));
+    	}
+        public function testNegative(){
+        	$number = new fooBar();
+        	$this ->assertIsNotInt($number->checkFooBar(-7));
+        }
+        public function testInvalidChar(){
+        	$number = new fooBar();
+        	$this -> assertIsNotInt($number->checkFooBar("test"));
+        }
+        public function testFoo(){
+        	$number = new fooBar();
+        	$this ->assertEquals($number->checkFooBar(3),"Foo");
+        }
+        public function testBar(){
+        	$number = new fooBar();
+        	$this -> assertEquals($number->checkFooBar(5),"Bar");
+        }
+        public function testFooBar(){
+            $number = new fooBar();
+            $this -> assertEquals($number->checkFooBar(15),"Foo Bar");
+        }
+
+    }
+
+
+
+ ?>

b9157f525407eca8d17d74172632558dd82b2a54
RihardsMacijauskis <71095635+RihardsMacijauskis@users.noreply.github.com>
2020-09-12 11:18:32 +0300

    Update fooBar.php
    Got rid of some bugs caused by unit testing

--- fooBar.php
+++ fooBar.php
@@ -1,24 +1,34 @@
 <?php
 class fooBar
 {
-public function checkFooBar($number){
-    //Check if a number is a positive integer
-    if ((is_int($number) || ctype_digit($number)) && (int)$number > 0 ) { 
-        //Check if a number is a multiple of 3
-        if (($number % 3) == 0) {
-            echo "Foo ";
+    function checkFooBar($number)
+    {
+        $result = "";
+        //Check if a number is a positive integer
+        if ((is_int($number) || ctype_digit($number)) && (int)$number > 0)
+        {
+            //Check if a number is a multiple of 3
+            if (($number % 3) == 0)
+            {
+                $result = "Foo ";
+            }
+            //Check if a number is a multiple of 5
+            if (($number % 5) == 0)
+            {
+                $result .= "Bar";
+            }
+            //Check if a number isn't a multiple of 3 or 5
+            if ((($number % 5) !== 0) && (($number % 3) !== 0))
+            {
+                $result = "$number";
+            }
         }
-        //Check if a number is a multiple of 5
-        if (($number % 5) == 0){
-            echo " Bar ";
+        else
+        {
+            $result = "Number is not a positive integer";
         }
-        //Check if a number isn't a multiple of 3 or 5
-        if ((($number % 5) !== 0) && (($number % 3) !== 0)) {
-            echo "$number";
-        } 
-    } else {
-        echo "Number is not a positive integer";
-    } 
+        return $result;
     }
 }
+
 ?>

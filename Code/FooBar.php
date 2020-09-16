fc9bc40c70de80e9ba24ce5d6e9ebbb8700c3181
Vladislavs Perkanuks <69954124+VladislavsPerkanuks@users.noreply.github.com>
2020-09-16 09:31:40 +0300

    Instead of multiple if's, "foreach" loop to prevent code duplication. Exception is now before initializing variables

--- Code/FooBar.php
+++ Code/FooBar.php
@@ -6,22 +6,21 @@ class FooBar
 {
     public function checker($number)
     {
-        $resultArray = array();
-
         if (!is_int($number) || $number < 0) {
             throw new InvalidArgumentException();
         }
 
-        if ($number % 3 == 0) {
-            $resultArray[0] = "Foo";
-        }
+        $resultArray = array();
+        $multiplierArray = array(
+            "Foo" => 3,
+            "Bar" => 5,
+            "Qix" => 7,
+        );
 
-        if ($number % 5 == 0) {
-            $resultArray[1] = "Bar";
-        }
-
-        if ($number % 7 == 0) {
-            $resultArray[2] = "Qix";
+        foreach ($multiplierArray as $name => $multiplier) {
+            if ($number % $multiplier == 0) {
+                array_push($resultArray, $name);
+            }
         }
 
         if (!empty($resultArray)) {

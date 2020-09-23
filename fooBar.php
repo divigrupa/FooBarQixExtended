a5f12cc0400685c6276aa786d6b930b7d90f1d32
RihardsMacijauskis <71095635+RihardsMacijauskis@users.noreply.github.com>
2020-09-23 13:22:34 +0300

    Update fooBar.php
    Changed class name so it complies with PSR guidelines, got rid of comments, whitespaces, edited the else statement so it throws an exception.

--- fooBar.php
+++ fooBar.php
@@ -1,23 +1,19 @@
 <?php
-class fooBar
+class FooBar
 {
     function checkFooBar($number)
     {
         $result = "";
-        //Check if a number is a positive integer
         if ((is_int($number) || ctype_digit($number)) && (int)$number > 0)
         {
-            //Check if a number is a multiple of 3
             if (($number % 3) == 0)
             {
-                $result = "Foo ";
+                $result = "Foo";
             }
-            //Check if a number is a multiple of 5 and concatenate it
             if (($number % 5) == 0)
             {
                 $result .= "Bar";
             }
-            //If a number isn't a multiple of 3 or 5 convert it to string
             if ((($number % 5) !== 0) && (($number % 3) !== 0))
             {
                 $result = strval($number);
@@ -25,7 +21,7 @@ class fooBar
         }
         else
         {
-            $result = "Number is not a positive integer";
+            throw new Exception();
         }
         return $result;
     }

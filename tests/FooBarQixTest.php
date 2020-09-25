015a887c78a7fe20b3785238d87563e79a93437f
Edgars <Edgars+GitHub@gaitenis.id.lv>
2020-09-25 19:18:26 +0300

    Unit tests were updated to introduce Qix
    Some existing test methods in `tests/FooBarQix.php` were updated to adjust
    their name or documentation block for the expected changes.

--- tests/FooBarQixTest.php
+++ tests/FooBarQixTest.php
@@ -62,6 +62,36 @@ final class FooBarQixTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * Test the given value if it is Foo, Bar and Qix
+     *
+     * @return void
+     */
+    public function testValueIsFooBarQix(): void
+    {
+        $this->assertSame("Foo, Bar, Qix", FooBarQix::process(210));
+    }
+
+    /**
+     * Test the given value if it is Foo and Qix
+     *
+     * @return void
+     */
+    public function testValueIsFooQix(): void
+    {
+        $this->assertSame("Foo, Qix", FooBarQix::process(42));
+    }
+
+    /**
+     * Test the given value if it is Bar and Qix
+     *
+     * @return void
+     */
+    public function testValueIsBarQix(): void
+    {
+        $this->assertSame("Bar, Qix", FooBarQix::process(70));
+    }
+
+    /**
      * Test the given value if it is both Foo and Bar
      *
      * @return void
@@ -92,11 +122,21 @@ final class FooBarQixTest extends PHPUnit\Framework\TestCase
     }
 
     /**
-     * Test an input when it is detected as neither Foo nor Bar
+     * Test the given value if it is detected as Qix
      *
      * @return void
      */
-    public function testValueIsNeitherFooNorBar(): void
+    public function testValueIsQix(): void
+    {
+        $this->assertSame("Qix", FooBarQix::process(14));
+    }
+
+    /**
+     * Test an input when it is not detected as Foo, Bar and Qix
+     *
+     * @return void
+     */
+    public function testValueIsNotFooBarQix(): void
     {
         $input = 1;
         $output = FooBarQix::process($input);
@@ -114,6 +154,10 @@ final class FooBarQixTest extends PHPUnit\Framework\TestCase
         $this->assertSame("2", FooBarQix::process("2"));
         $this->assertSame("Foo", FooBarQix::process("12"));
         $this->assertSame("Bar", FooBarQix::process("20"));
+        $this->assertSame("Qix", FooBarQix::process("28"));
         $this->assertSame("Foo, Bar", FooBarQix::process("30"));
+        $this->assertSame("Foo, Qix", FooBarQix::process("63"));
+        $this->assertSame("Bar, Qix", FooBarQix::process("140"));
+        $this->assertSame("Foo, Bar, Qix", FooBarQix::process("315"));
     }
 }

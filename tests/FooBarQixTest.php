64d2fb7360916d026dc2bd9f3e5c73243d8d299a
Edgars <Edgars+GitHub@gaitenis.id.lv>
2020-09-26 13:38:07 +0300

    Unit tests were updated for Foo, Bar and Qix occurence implementation

--- tests/FooBarQixTest.php
+++ tests/FooBarQixTest.php
@@ -62,77 +62,303 @@ final class FooBarQixTest extends PHPUnit\Framework\TestCase
     }
 
     /**
-     * Test the given value if it is Foo, Bar and Qix
+     * Test the given values if they are completely Foo, Bar and Qix
+     *
+     * Test the given integers if they are divisible by Foo, Bar and Qix and if
+     * Foo, Bar and Qix digits occur in them.
      *
      * @return void
      */
-    public function testValueIsFooBarQix(): void
+    public function testValueHasMultipliersAndContainsFooBarAndQix(): void
+    {
+        $this->assertSame(
+            "Foo, Bar, Qix, Qix, Foo, Bar",
+            FooBarQix::process(735)
+        );
+        $this->assertSame(
+            "Foo, Bar, Qix, Foo, Bar, Qix",
+            FooBarQix::process(3570)
+        );
+        $this->assertSame(
+            "Foo, Bar, Qix, Foo, Qix, Bar",
+            FooBarQix::process(3675)
+        );
+    }
+
+    /**
+     * Test the given value if it is divisible by and contains Foo and Bar
+     *
+     * The integer must neither be divisible by nor contain Qix.
+     *
+     * @return void
+     */
+    public function testValueHasMultipliersAndContainsFooAndBar(): void
+    {
+        $this->assertSame("Foo, Bar, Foo, Bar", FooBarQix::process(135));
+        $this->assertSame("Foo, Bar, Foo, Foo, Bar", FooBarQix::process(1335));
+        $this->assertSame("Foo, Bar, Bar, Foo", FooBarQix::process(1530));
+    }
+
+    /**
+     * Test the given value if it is divisible by and contains Foo and Qix
+     *
+     * The integer must neither be divisible by nor contain Bar.
+     *
+     * @return void
+     */
+    public function testValueHasMultipliersAndContainsFooAndQix(): void
+    {
+        $this->assertSame("Foo, Qix, Qix, Foo", FooBarQix::process(273));
+        $this->assertSame("Foo, Qix, Foo, Qix", FooBarQix::process(378));
+        $this->assertSame("Foo, Qix, Foo, Qix, Foo", FooBarQix::process(2373));
+    }
+
+    /**
+     * Test the given value if it is divisible by and contains Bar and Qix
+     *
+     * The integer must neither be divisible by nor contain Foo.
+     *
+     * @return void
+     */
+    public function testValueHasMultipliersAndContainsBarAndQix(): void
+    {
+        $this->assertSame("Bar, Qix, Qix, Bar", FooBarQix::process(175));
+        $this->assertSame("Bar, Qix, Bar, Qix, Bar", FooBarQix::process(1575));
+    }
+
+    /**
+     * Test the given value if it only is divisible by and contains Foo
+     *
+     * The integer must neither be divisible by nor contain Bar and Qix.
+     *
+     * @return void
+     */
+    public function testValueHasMultiplierAndContainsFoo(): void
+    {
+        $this->assertSame("Foo, Foo", FooBarQix::process(36));
+        $this->assertSame("Foo, Foo, Foo", FooBarQix::process(33));
+    }
+
+    /**
+     * Test the given value if it only is divisible by and contains Bar
+     *
+     * The integer must neither be divisible by nor contain Foo and Qix.
+     *
+     * @return void
+     */
+    public function testValueHasMultiplierAndContainsBar(): void
+    {
+        $this->assertSame("Bar, Bar", FooBarQix::process(50));
+        $this->assertSame("Bar, Bar, Bar", FooBarQix::process(55));
+    }
+
+    /**
+     * Test the given value if it only is divisible by and contains Qix
+     *
+     * The integer must neither be divisible by nor contain Foo and Bar.
+     *
+     * @return void
+     */
+    public function testValueHasMultiplierAndContainsQix(): void
+    {
+        $this->assertSame("Qix, Qix", FooBarQix::process(217));
+        $this->assertSame("Qix, Qix, Qix", FooBarQix::process(77));
+    }
+
+    /**
+     * Test the given value if it only is divisible by Foo, Bar and Qix
+     *
+     * The integer must not contain Foo, Bar and Qix.
+     *
+     * @return void
+     */
+    public function testValueOnlyHasMultipliersFooBarAndQix(): void
     {
         $this->assertSame("Foo, Bar, Qix", FooBarQix::process(210));
     }
 
     /**
-     * Test the given value if it is Foo and Qix
+     * Test the given value if it only is divisible by both Foo and Bar
+     *
+     * The integer must neither contain Foo, Bar and Qix nor be divisible by
+     * Qix.
      *
      * @return void
      */
-    public function testValueIsFooQix(): void
+    public function testValueOnlyHasMultipliersFooAndBar(): void
+    {
+        $this->assertSame("Foo, Bar", FooBarQix::process(60));
+    }
+
+    /**
+     * Test the given value if it only is divisible by Foo and Qix
+     *
+     * The integer must neither contain Foo, Bar and Qix nor be divisible by
+     * Bar.
+     *
+     * @return void
+     */
+    public function testValueOnlyHasMultipliersFooAndQix(): void
     {
         $this->assertSame("Foo, Qix", FooBarQix::process(42));
     }
 
     /**
-     * Test the given value if it is Bar and Qix
+     * Test the given value if it only is divisible by Bar and Qix
+     *
+     * The integer must neither contain Foo, Bar and Qix nor be divisible by
+     * Foo.
      *
      * @return void
      */
-    public function testValueIsBarQix(): void
+    public function testValueOnlyHasMultipliersBarAndQix(): void
     {
-        $this->assertSame("Bar, Qix", FooBarQix::process(70));
+        $this->assertSame("Bar, Qix", FooBarQix::process(280));
     }
 
     /**
-     * Test the given value if it is both Foo and Bar
+     * Test the given value if it only is divisible by Foo
+     *
+     * The integer must neither contain Foo, Bar and Qix nor be divisible by
+     * Bar and Qix.
      *
      * @return void
      */
-    public function testValueIsFooBar(): void
-    {
-        $this->assertSame("Foo, Bar", FooBarQix::process(15));
-    }
-
-    /**
-     * Test the given value if it is detected as Foo
-     *
-     * @return void
-     */
-    public function testValueIsFoo(): void
+    public function testValueOnlyHasMultiplierFoo(): void
     {
         $this->assertSame("Foo", FooBarQix::process(6));
     }
 
     /**
-     * Test the given value if it is detected as Bar
+     * Test the given value if it only is divisible by Bar
+     *
+     * The integer must neither contain Foo, Bar and Qix nor be divisible by
+     * Bar and Qix.
      *
      * @return void
      */
-    public function testValueIsBar(): void
+    public function testValueOnlyHasMultiplierBar(): void
     {
         $this->assertSame("Bar", FooBarQix::process(10));
     }
 
     /**
-     * Test the given value if it is detected as Qix
+     * Test the given value if it only is divisible by Qix
+     *
+     * The integer must neither contain Foo, Bar and Qix nor be divisible by
+     * Foo and Bar.
      *
      * @return void
      */
-    public function testValueIsQix(): void
+    public function testValueOnlyHasMultiplierQix(): void
     {
         $this->assertSame("Qix", FooBarQix::process(14));
     }
 
     /**
-     * Test an input when it is not detected as Foo, Bar and Qix
+     * Test the given value if it only contains Foo, Bar and Qix
+     *
+     * The integer must not be divisible by Foo, Bar and Qix.
+     *
+     * @return void
+     */
+    public function testValueOnlyContainsFooBarAndQix(): void
+    {
+        $this->assertSame("34567, Foo, Bar, Qix", FooBarQix::process(34567));
+        $this->assertSame("456731, Bar, Qix, Foo", FooBarQix::process(456731));
+        $this->assertSame("7354, Qix, Foo, Bar", FooBarQix::process(7354));
+        $this->assertSame("3754, Foo, Qix, Bar", FooBarQix::process(3754));
+        $this->assertSame("5732, Bar, Qix, Foo", FooBarQix::process(5732));
+        $this->assertSame("67532, Qix, Bar, Foo", FooBarQix::process(67532));
+    }
+
+    /**
+     * Test the given value if it only contains Foo and Bar
+     *
+     * The integer must neither be divisible by Foo, Bar and Qix nor contain
+     * Qix.
+     *
+     * @return void
+     */
+    public function testValueOnlyContainsFooAndBar(): void
+    {
+        $this->assertSame("352, Foo, Bar", FooBarQix::process(352));
+        $this->assertSame("53, Bar, Foo", FooBarQix::process(53));
+        $this->assertSame("353, Foo, Bar, Foo", FooBarQix::process(353));
+    }
+
+    /**
+     * Test the given value if it contains Foo and Qix
+     *
+     * The integer must neither be divisible by Foo, Bar and Qix nor contain
+     * Bar.
+     *
+     * @return void
+     */
+    public function testValueOnlyContainsFooAndQix(): void
+    {
+        $this->assertSame("37, Foo, Qix", FooBarQix::process(37));
+        $this->assertSame("73, Qix, Foo", FooBarQix::process(73));
+    }
+
+    /**
+     * Test the given value if it contains Bar and Qix
+     *
+     * The integer must neither be divisible by Foo, Bar and Qix nor contain
+     * Foo.
+     *
+     * @return void
+     */
+    public function testValueOnlyContainsBarAndQix(): void
+    {
+        $this->assertSame("571, Bar, Qix", FooBarQix::process(571));
+        $this->assertSame("754, Qix, Bar", FooBarQix::process(754));
+    }
+
+    /**
+     * Test the given value if it only contains Foo
+     *
+     * The integer must neither be divisible by Foo, Bar and Qix nor contain
+     * Bar and Qix.
+     *
+     * @return void
+     */
+    public function testValueOnlyContainsFoo(): void
+    {
+        $this->assertSame("31, Foo", FooBarQix::process(31));
+    }
+
+    /**
+     * Test the given value if it only contains Bar
+     *
+     * The integer must neither be divisible by Foo, Bar and Qix nor contain
+     * Foo and Qix.
+     *
+     * @return void
+     */
+    public function testValueOnlyContainsBar(): void
+    {
+        $this->assertSame("52, Bar", FooBarQix::process(52));
+    }
+
+    /**
+     * Test the given value if it only contains Qix
+     *
+     * The integer must neither be divisible by Foo, Bar and Qix nor contain
+     * Foo and Bar.
+     *
+     * @return void
+     */
+    public function testValueOnlyContainsQix(): void
+    {
+        $this->assertSame("71, Qix", FooBarQix::process(71));
+    }
+
+    /**
+     * Test an input when it is not detected as nor contains Foo, Bar and Qix
+     *
+     * Test if the given integer is not divisible by Foo, Bar and Qix and it
+     * does not contain Foo, Bar and Qix.
      *
      * @return void
      */
@@ -159,5 +385,7 @@ final class FooBarQixTest extends PHPUnit\Framework\TestCase
         $this->assertSame("Foo, Qix", FooBarQix::process("63"));
         $this->assertSame("Bar, Qix", FooBarQix::process("140"));
         $this->assertSame("Foo, Bar, Qix", FooBarQix::process("315"));
+        $this->assertSame("Foo, Bar, Foo, Bar", FooBarQix::process("345"));
+        $this->assertSame("13571, Foo, Bar, Qix", FooBarQix::process("13571"));
     }
 }

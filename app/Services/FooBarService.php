5f917ad012fa82b7a10de8d0095f9a3bcfe52764
Reinis Danne <rei4dan@gmail.com>
2021-05-12 01:35:17 +0300

    Services: Implement FooBarService
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- app/Services/FooBarService.php
+++ app/Services/FooBarService.php
@@ -4,10 +4,33 @@ declare(strict_types=1);
 
 namespace FooBar\Services;
 
+use InvalidArgumentException;
+
 class FooBarService
 {
+    private array $map = [
+        3 => 'Foo',
+        5 => 'Bar',
+    ];
+
     public function get(int $number): string
     {
-        return '';
+        if ($number < 0) {
+            throw new InvalidArgumentException("Expected positive integer");
+        }
+
+        $result = '';
+
+        foreach ($this->map as $key => $value) {
+            if ($number % $key === 0) {
+                $result .= $value;
+            }
+        }
+
+        if ($result === '') {
+            return (string)$number;
+        }
+
+        return $result;
     }
 }

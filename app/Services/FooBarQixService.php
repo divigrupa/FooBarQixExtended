11463b2209a131e2e9aa274ba23349dd28c4f3cf
Janis <jaanis.jankovs@gmail.com>
2021-10-11 09:55:42 +0300

    Ad getOccurrences method to our FooBarQix service, check if tests ok

--- app/Services/FooBarQixService.php
+++ app/Services/FooBarQixService.php
@@ -14,11 +14,25 @@ class FooBarQixService
     public function getMultiples(int $givenNumber): string
     {
         $result = [];
-        foreach(self::RETURN_VALUES as $key => $value)
+        foreach(self::RETURN_VALUES as $key => $returnValue)
         {
             if($givenNumber % $key === 0)
             {
-                $result[] = $value;
+                $result[] = $returnValue;
+            }
+        }
+        return empty($result) ? (string) $givenNumber : implode(', ', $result);
+    }
+
+    public function getOccurrences(int $givenNumber): string
+    {
+        $result = [];
+        $splittedGivenNumber = str_split($givenNumber,1);
+        foreach(self::RETURN_VALUES as $key => $returnValue)
+        {
+            if(in_array($key, $splittedGivenNumber))
+            {
+                $result[] = $returnValue;
             }
         }
         return empty($result) ? (string) $givenNumber : implode(', ', $result);

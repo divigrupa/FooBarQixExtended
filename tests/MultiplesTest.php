35ba8359c9a0b15226c82eea9a808faa07ffb196
Janis <jaanis.jankovs@gmail.com>
2021-10-11 10:05:36 +0300

    dd tests for our new InfQixFoo service

--- tests/MultiplesTest.php
+++ tests/MultiplesTest.php
@@ -9,36 +9,91 @@ class FooBarTest extends TestCase
 {
     public function testIfReturnsFoo()
     {
-        $this->assertEquals('Foo', (new FooBarQixService)->getMultiples(3321));
+        $this->assertEquals("Foo", (new FooBarQixService)->getMultiples(3));
+        $this->assertEquals("Foo", (new InfQixFooService)->getMultiples(6));
     }
 
     public function testIfReturnsBar()
     {
-        $this->assertEquals('Bar', (new FooBarQixService)->getMultiples(5));
-    }
-
-    public function testIfReturnsFooBar()
-    {
-        $this->assertEquals('Foo, Bar', (new FooBarQixService)->getMultiples(15));
+        $this->assertEquals("Bar", (new FooBarQixService)->getMultiples(5));
+        $this->assertEquals("Bar", (new FooBarQixService)->getMultiples(10));
     }
 
     public function testIfReturnsQix()
     {
-        $this->assertEquals('Qix', (new FooBarQixService)->getMultiples(7));
+        $this->assertEquals("Qix", (new FooBarQixService)->getMultiples(7));
+        $this->assertEquals('Qix', (new InfQixFooService)->getMultiples(14));
     }
 
-    public function testIfReturnsFooQix()
+    public function testIfReturnsFooBar()
     {
-        $this->assertEquals('Foo, Qix', (new FooBarQixService)->getMultiples(21));
+        $this->assertEquals("Foo, Bar", (new FooBarQixService)->getMultiples(15));
+        $this->assertEquals("Foo, Bar", (new FooBarQixService)->getMultiples(10995));
     }
 
     public function testIfReturnsBarQix()
     {
-        $this->assertEquals('Bar, Qix', (new FooBarQixService)->getMultiples(35));
+        $this->assertEquals("Bar, Qix", (new FooBarQixService)->getMultiples(35));
+        $this->assertEquals("Bar, Qix", (new FooBarQixService)->getMultiples(10990));
+
+    }
+
+    public function testIfReturnsFooQix()
+    {
+        $this->assertEquals("Foo, Qix", (new FooBarQixService)->getMultiples(21));
+        $this->assertEquals("Foo, Qix", (new FooBarQixService)->getMultiples(10962));
+
     }
 
     public function testIfReturnsFooBarQix()
     {
-        $this->assertEquals('Foo, Bar, Qix', (new FooBarQixService)->getMultiples(105));
+        $this->assertEquals("Foo, Bar, Qix", (new FooBarQixService)->getMultiples(105));
+        $this->assertEquals("Foo, Bar, Qix", (new FooBarQixService)->getMultiples(10920));
+
+    }
+
+    public function testIfReturnsNumber()
+    {
+        $this->assertEquals('2', (new FooBarQixService)->getMultiples(2));
+        $this->assertEquals('1013', (new FooBarQixService)->getMultiples(1013));
+
+    }
+
+    public function testIfReturnsGivenNumberAsString()
+    {
+        $this->assertIsString((new FooBarQixService)->getMultiples(2));
+        $this->assertIsString((new FooBarQixService)->getMultiples(586));
+
+    }
+
+    public function testIfReturnsInf()
+    {
+        $this->assertEquals('Inf', (new InfQixFooService)->getMultiples(8));
+        $this->assertEquals('Inf', (new InfQixFooService)->getMultiples(10480));
+
+    }
+
+    public function testIfReturnsInfQuix()
+    {
+        $this->assertEquals('Inf; Qix', (new InfQixFooService)->getMultiples(56));
+        $this->assertEquals('Inf; Qix', (new InfQixFooService)->getMultiples(10864));
+    }
+
+    public function testIfReturnsInfFoo()
+    {
+        $this->assertEquals('Inf; Foo', (new InfQixFooService)->getMultiples(48));
+        $this->assertEquals('Inf; Foo', (new InfQixFooService)->getMultiples(10968));
+    }
+
+    public function testIfReturnQixFoo()
+    {
+        $this->assertEquals('Qix; Foo', (new InfQixFooService)->getMultiples(42));
+        $this->assertEquals('Qix; Foo', (new InfQixFooService)->getMultiples(10983));
+    }
+
+    public function testIfReturnInfQixFoo()
+    {
+        $this->assertEquals('Inf; Qix; Foo', (new InfQixFooService)->getMultiples(168));
+        $this->assertEquals('Inf; Qix; Foo', (new InfQixFooService)->getMultiples(10920));
     }
 }
--- tests/OccurrencesTest.php
+++ tests/OccurrencesTest.php
@@ -9,41 +9,83 @@ class OccurrencesTest extends TestCase
 {
     public function testIfReturnsFoo()
     {
-        $this->assertEquals('Foo', (new FooBarQixService)->getOccurrences(293));
+        $this->assertEquals("Foo", (new FooBarQixService)->getOccurrences(234));
+        $this->assertEquals("Foo", (new FooBarQixService)->getOccurrences(343));
+
+        $this->assertEquals("Foo", (new InfQixFooService)->getOccurrences(2364));
+        $this->assertEquals("Foo", (new InfQixFooService)->getOccurrences(223));
     }
 
     public function testIfReturnsBar()
     {
-        $this->assertEquals('Bar', (new FooBarQixService)->getOccurrences(5598));
-    }
-
-    public function testIfReturnsFooBar()
-    {
-        $this->assertEquals('Foo, Bar', (new FooBarQixService)->getOccurrences(395));
+        $this->assertEquals("Bar", (new FooBarQixService)->getOccurrences(254));
+        $this->assertEquals("Bar", (new FooBarQixService)->getOccurrences(529));
     }
 
     public function testIfReturnsQix()
     {
-        $this->assertEquals('Qix', (new FooBarQixService)->getOccurrences(7779));
+        $this->assertEquals("Qix", (new FooBarQixService)->getOccurrences(4997));
+        $this->assertEquals("Qix", (new FooBarQixService)->getOccurrences(27766));
+
+        $this->assertEquals('Qix', (new InfQixFooService)->getOccurrences(794));
+        $this->assertEquals('Qix', (new InfQixFooService)->getOccurrences(2222277));
     }
 
-    public function testIfReturnsFooQix()
+    public function testIfReturnsFooBar()
     {
-        $this->assertEquals('Foo, Qix', (new FooBarQixService)->getOccurrences(9973));
+        $this->assertEquals("Foo, Bar", (new FooBarQixService)->getOccurrences(3295));
+        $this->assertEquals("Foo, Bar", (new FooBarQixService)->getOccurrences(10395));
     }
 
     public function testIfReturnsBarQix()
     {
-        $this->assertEquals('Bar, Qix', (new FooBarQixService)->getOccurrences(97245));
+        $this->assertEquals("Bar, Qix", (new FooBarQixService)->getOccurrences(157));
+        $this->assertEquals("Bar, Qix", (new FooBarQixService)->getOccurrences(759));
+
+    }
+
+    public function testIfReturnsFooQix()
+    {
+        $this->assertEquals("Foo, Qix", (new FooBarQixService)->getOccurrences(723));
+        $this->assertEquals("Foo, Qix", (new FooBarQixService)->getOccurrences(3666667));
+
     }
 
     public function testIfReturnsFooBarQix()
     {
-        $this->assertEquals('Foo, Bar, Qix', (new FooBarQixService)->getOccurrences(75322));
+        $this->assertEquals("Foo, Bar, Qix", (new FooBarQixService)->getOccurrences(753));
+        $this->assertEquals("Foo, Bar, Qix", (new FooBarQixService)->getOccurrences(5299387));
+
     }
 
-    public function testIfReturnsNumberAsString()
+    public function testIfReturnsInf()
     {
-        $this->assertIsString((new FooBarQixService)->getOccurrences(2));
+        $this->assertEquals('Inf', (new InfQixFooService)->getOccurrences(8));
+        $this->assertEquals('Inf', (new InfQixFooService)->getOccurrences(8229));
+
+    }
+
+    public function testIfReturnsInfQuix()
+    {
+        $this->assertEquals('Inf; Qix', (new InfQixFooService)->getOccurrences(798));
+        $this->assertEquals('Inf; Qix', (new InfQixFooService)->getOccurrences(77258));
+    }
+
+    public function testIfReturnsInfFoo()
+    {
+        $this->assertEquals('Inf; Foo', (new InfQixFooService)->getOccurrences(3388));
+        $this->assertEquals('Inf; Foo', (new InfQixFooService)->getOccurrences(89243));
+    }
+
+    public function testIfReturnQixFoo()
+    {
+        $this->assertEquals('Qix; Foo', (new InfQixFooService)->getOccurrences(7773));
+        $this->assertEquals('Qix; Foo', (new InfQixFooService)->getOccurrences(32697));
+    }
+
+    public function testIfReturnInfQixFoo()
+    {
+        $this->assertEquals('Inf; Qix; Foo', (new InfQixFooService)->getOccurrences(873));
+        $this->assertEquals('Inf; Qix; Foo', (new InfQixFooService)->getOccurrences(392758));
     }
 }

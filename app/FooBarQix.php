fa961e9ee99491e8ff3d544a72f66489d31c02f8
Aigars <aigars.liskovskis@gmail.com>
2022-04-17 20:27:48 +0300

    Implement step 1 method: multiple 3, 5

--- app/FooBarQix.php
+++ app/FooBarQix.php
@@ -5,5 +5,25 @@ namespace App;
 
 class FooBarQix
 {
+    private array $conditions;
+    private array $output = [];
+    private string $separator = ', ';
 
+    public function __construct(array $conditions)
+    {
+        $this->conditions = $conditions;
+    }
+
+    public function multiple(int $number): string
+    {
+        foreach ($this->conditions as $key => $value) {
+            if ($number % $key === 0) {
+                $this->output[] = $value;
+            }
+        }
+        if (empty($this->output)) {
+            return $number;
+        }
+        return implode($this->separator, $this->output);
+    }
 }

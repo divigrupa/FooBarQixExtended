5ba4df0756a7ce4592093586d974d2afce1cb996
KristapsRigasts <kristapsrigasts@inbox.lv>
2022-05-03 09:53:21 +0300

    Added test cases for multiple of 7 or 3 and 7 or 5 and 7 or 3 and 5 and 7

--- tests/FooBarQixTest.php
+++ tests/FooBarQixTest.php
@@ -42,4 +42,40 @@ class FooBarQixTest extends TestCase
         $this->assertSame('4', $stepOne->multiple(4));
         $this->assertSame('16', $stepOne->multiple(16));
     }
+
+    public function testMultipleOfSeven()
+    {
+        $stepTwo = new FooBarQix();
+
+        $this->assertSame('Qix', $stepTwo->multiple(7));
+        $this->assertSame('Qix', $stepTwo->multiple(28));
+        $this->assertSame('Qix', $stepTwo->multiple(42));
+    }
+
+    public function testMultipleOfThreeAndSeven()
+    {
+        $stepTwo = new FooBarQix();
+
+        $this->assertSame('Foo, Qix', $stepTwo->multiple(21));
+        $this->assertSame('Foo, Qix', $stepTwo->multiple(63));
+        $this->assertSame('Foo, Qix', $stepTwo->multiple(126));
+    }
+
+    public function testMultipleOfFiveAndSeven()
+    {
+        $stepTwo = new FooBarQix();
+
+        $this->assertSame('Bar, Qix', $stepTwo->multiple(35));
+        $this->assertSame('Bar, Qix', $stepTwo->multiple(70));
+        $this->assertSame('Bar, Qix', $stepTwo->multiple(140));
+    }
+
+    public function testMultipleOfThreeAndFiveAndSeven()
+    {
+        $stepTwo = new FooBarQix();
+
+        $this->assertSame('Foo, Bar, Qix', $stepTwo->multiple(105));
+        $this->assertSame('Foo, Bar, Qix', $stepTwo->multiple(420));
+        $this->assertSame('Foo, Bar, Qix', $stepTwo->multiple(630));
+    }
 }

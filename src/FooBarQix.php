c391249bd72c4c97e91550ff53ed3442d5ec1443
Alexey Zrelov <azheimdall@gmail.com>
2022-05-07 10:46:54 +0300

    Add implementation for FooBarTest

--- src/FooBarQix.php
+++ src/FooBarQix.php
@@ -4,5 +4,24 @@ namespace App;
 
 class FooBarQix
 {
+    public function answer($number)
+    {
+        if ($number % 105 === 0) {
+            return "Foo, Bar, Qix";
+        }
+        if ($number % 15 === 0) {
+            return "Foo, Bar";
+        }
+        if ($number % 7 === 0) {
+            return "Qix";
+        }
+        if ($number % 5 === 0) {
+            return "Bar";
+        }
+        if ($number % 3 === 0) {
+            return "Foo";
+        }
 
+        return $number;
+    }
 }

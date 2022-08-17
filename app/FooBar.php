772689726d99281a2735da2bde9ba538d9924de4
Juris B <juris.bumaniss@gmail.com>
2022-08-17 12:23:15 +0300

    Step 1 implementation code added.

--- app/FooBar.php
+++ app/FooBar.php
@@ -5,10 +5,33 @@ namespace App;
 
 class FooBar
 {
+    private int $numEnd;
+    private int $numStart;
+    private int $fooNumber = 3;
+    private int $barNumber = 5;
+    private string $foo = 'Foo';
+    private string $bar = 'Bar';
+
+
+    public function __construct(int $numEnd)
+    {
+        $this->numEnd = $numEnd;
+        $this->numStart =$numEnd;
+    }
 
     public function start(): string
     {
-
+        $returnString = '';
+        for ($i = $this->numStart; $i <= $this->numEnd; $i++) {
+            if ($i % $this->fooNumber == 0) {
+                $returnString .= $this->foo . ' ';
+            } elseif ($i % $this->barNumber == 0) {
+                $returnString .= $this->bar . ' ';
+            } else {
+                $returnString .= $i . ' ';
+            }
+        }
+        return trim($returnString);
     }
 
 

efe8b0ee0fc088ee5bb78f43622657c4b94cd55a
Juris B <juris.bumaniss@gmail.com>
2022-08-17 21:03:38 +0300

    Step 3 test modified for step 3 requirements

--- tests/FooBarTest.php
+++ tests/FooBarTest.php
@@ -3,28 +3,33 @@
 
 use App\FooBar;
 
+test('test case: positive number', function () {
+    $fizzBuzz = new FooBar(4);
+    expect($fizzBuzz->start())->toBeGreaterThan(0);
+});
+
 test('test case: other numbers', function () {
     $fizzBuzz = new FooBar(4);
     expect($fizzBuzz->start())->toEqual('4');
 });
 
-test('test case: "Foo"', function () {
+test('test case: "3 Foo"', function () {
     $fizzBuzz = new FooBar(3);
-    expect($fizzBuzz->start())->toEqual('Foo');
+    expect($fizzBuzz->start())->toEqual('3 Foo');
 });
 
-test('test case: "Bar"', function () {
+test('test case: "5 Bar"', function () {
     $fizzBuzz = new FooBar(5);
-    expect($fizzBuzz->start())->toEqual('Bar');
+    expect($fizzBuzz->start())->toEqual('5 Bar');
 });
 
-test('test case: "Qix"', function () {
+test('test case: "7 Qix"', function () {
     $fizzBuzz = new FooBar(7);
-    expect($fizzBuzz->start())->toEqual('Qix');
+    expect($fizzBuzz->start())->toEqual('7 Qix');
 });
 
 test('test case: natural order "Foo, Bar, Qix"', function () {
     $fizzBuzz = new FooBar(105);
-    expect($fizzBuzz->start())->toEqual('Foo Bar Qix');
+    expect($fizzBuzz->start())->toEqual('105 Foo Bar Qix');
 });
 

ad1e29c794fbcc5642a5cfac3a2d79f597bbb991
Toms Bēmis <tomsbemis@gmail.com>
2022-08-20 14:37:53 +0300

    Step 3 implementation:  - occurrences function validates input;  - returns all the transformations if any of the digits of input number match the defined transformations.

--- app/Services/OccurrencesService.php
+++ app/Services/OccurrencesService.php
@@ -7,8 +7,62 @@ namespace App\Services;
 class OccurrencesService extends NumberService
 {
 
+    private $digits;   //Key value pairs with digit values and their associated output
+
+    function __construct(){
+        $this->digits = collect([
+            [
+                'digit' => 5,
+                'output' => 'Bar'
+            ],
+            [
+                'digit' => 3,
+                'output' => 'Foo'
+            ],
+            [
+                'digit' => 7,
+                'output' => 'Qix'
+            ],
+        ]);
+    }
+
+    /*
+     * Transforms input number digits into associated strings and
+     * returns an array of all the transformations if any were present.
+     * Also validates that input number is a positive integer.
+     */
     public function occurrences($input_number){
 
-        return null;
+        //Validate
+        if(!$this->is_input_valid($input_number)) return [
+            'success' => false,
+            'input'=>$input_number,
+            'error' => 'Input must be a positive integer'
+        ];
+
+        //Split input number into digits
+        $input_digits = str_split((string)$input_number);
+
+        $result = [];
+        // Go through each digit in input number
+        foreach($input_digits as $input_digit){
+
+            //Find matching transformation digit for current input digit
+            $transformation_digit = null;
+            foreach($this->digits as $defined_digit){
+
+                if((integer)$input_digit == $defined_digit['digit']) {
+                    //Transformation digit matches, append output to array
+                    array_push($result,$defined_digit['output']);
+                    break;
+                }
+            }
+        }
+
+        return [
+            'success'=>true,
+            'input'=>$input_number,
+            'result'=>$result
+        ];
     }
 }

eb2dc8fe6304e3934a9a6d4ffeb83027a406baad
Toms Bēmis <tomsbemis@gmail.com>
2022-08-20 14:05:47 +0300

    Step 3 Tests Fail:  - OccurrencesService.php with one function that always returns null;  - Tests that go through arranging step but fail asserting.

---
+++ tests/Unit/Step3/Step3Test.php
@@ -0,0 +1,268 @@
+<?php
+
+namespace Tests\Unit\Step3;
+
+use App\Services\OccurrencesService;
+use Facade\Ignition\Support\Packagist\Package;
+use Tests\TestCase;
+
+class Step3Test extends TestCase
+{
+
+    const MAX_INT = 9223372036854775807;    //Upper limit of integer data type
+    private $number_range;  //Inputs that get filtered and rejected before passing them through the service
+    private $digits;   //Different multipliers and their expected outputs according to the requirements
+    private $occurrences_service; //The class instance that will produce the actual output
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        //Set up ranges of numbers to work with
+        $this->number_range = array_merge(
+            range(0,30),                                        //Low range
+            range(4611686018427387888,4611686018427387933),     //Mid range
+            range(self::MAX_INT - 30,self::MAX_INT)             //High range
+        );
+
+        $this->digits = [
+            [
+                'digit' => 5,
+                'output' => 'Bar'
+            ],
+            [
+                'digit' => 3,
+                'output' => 'Foo'
+            ],
+            [
+                'digit' => 7,
+                'output' => 'Qix'
+            ],
+        ];
+
+        //Create the service to use its functions
+        $this->occurrences_service = new OccurrencesService();
+    }
+
+
+
+    /**
+     * Provide an invalid integer and expect an error message
+     *
+     * @test
+     * @return void
+     */
+    public function invalid_integer_test()
+    {
+        $invalid_inputs = [
+            -1,     //Negative integer,
+            0.5,    //Positive floating point number,
+            "Abc",  //String
+        ];
+        foreach ($invalid_inputs as $invalid_input){
+            $this->assert_invalid_input($invalid_input);
+        }
+
+    }
+
+    private function assert_invalid_input($input){
+        $this->assertSame([
+            'success'=>false,
+            'input'=>$input,
+            'error'=>'Input must be a positive integer'
+        ], $this->occurrences_service->occurrences($input));
+    }
+
+
+    /**
+     * Pass each defined digit to the occurrences function for numbers where
+     * the digits occur once and multiple times and compare the function output.
+     *
+     * @test
+     * @return void
+     */
+    public function individual_digits_test(){
+
+        //One output element
+        //Iterate over each digit and compare if the output matches the defined key value pairs
+        foreach ($this->digits as $digit){
+
+            $this->assertSame(
+                [
+                    'success'=>true,
+                    'input'=>$digit['digit'],
+                    'result'=>[$digit['output']]    //Expected one output element in array
+                ],
+                $this->occurrences_service->occurrences($digit['digit'])    //Actual
+            );
+        }
+
+        //Multiple output elements
+        //Multiply digit by 111 to get three output elements that are the same
+        foreach ($this->digits as $digit){
+
+            $input_number = $digit['digit'] * 111;
+
+            $this->assertSame(
+                [
+                    'success'=>true,
+                    'input'=>$input_number,
+                    'result'=>[             //Expected three output elements in array
+                        $digit['output'],
+                        $digit['output'],
+                        $digit['output']
+                    ]
+                ],
+                $this->occurrences_service->occurrences($input_number)    //Actual
+            );
+        }
+    }
+
+
+    /**
+     * Create a number by concatenating defined digit-output pairs,
+     * from first to last and in reveres. Compare the occurrences function
+     * output to output expected for created numbers.
+     *
+     * @test
+     * @return void
+     */
+    public function mixed_digits_test(){
+
+        //Number and its expected output when combining digits from first to last
+        $digits_mix = collect($this->digits)->pluck('digit')->toArray();
+        $outputs_mix = collect($this->digits)->pluck('output')->toArray();
+
+        $input_number = (int)collect($digits_mix)->join('');    //Convert array to integer
+        $this->assertSame(
+            [
+                'success'=>true,
+                'input'=>$input_number,
+                'result'=>$outputs_mix
+            ],
+            $this->occurrences_service->occurrences($input_number)    //Actual when concatenating each digit into a single number
+        );
+
+        //Number and its expected output when combining digits from last to first
+        $reverse_digits_mix = collect($digits_mix)->reverse();
+        $reverse_outputs_mix = collect($outputs_mix)->reverse()->toArray();
+
+        $input_number = (int)collect($reverse_digits_mix)->join('');    //Convert array to integer
+
+        $this->assertSame(
+            [
+                'success'=>true,
+                'input'=>$input_number,
+                'result'=>$reverse_outputs_mix
+            ],
+            $this->occurrences_service->occurrences($input_number)    //Actual when concatenating each digit into a single number
+        );
+    }
+
+
+    /**
+     * Create input numbers that are expected to have no transformations and test
+     * if function returns an empty array
+     *
+     * @test
+     * @return void
+     */
+    public function no_transformation_test(){
+
+        //Create an array of numbers that have no digits for transformation
+        $no_transformation_numbers = collect($this->number_range);
+        foreach ($this->digits as $digit){
+
+            //Reject the numbers that when converted to array contain the current digit
+            $no_transformation_numbers = $no_transformation_numbers->reject(function ($number) use ($digit) {
+                return collect(str_split((string)$number))  //Split number by digit
+                    ->contains((string)$digit['digit']);    //Compare with current digit
+            });
+        }
+
+        //Go through each number that has no digits for transformation and compare output
+        foreach ($no_transformation_numbers as $no_transformation_number){
+            $this->assertSame(
+                [
+                    'success'=>true,
+                    'input'=>$no_transformation_number,
+                    'result'=>[]
+                ], //No output if number has no defined digits for transformation
+                $this->occurrences_service->occurrences($no_transformation_number)
+            );
+        }
+    }
+
+
+    /**
+     * Create an input number that has both digits with defined transformations
+     * and a digit without defined transformation. Compare if output skips the digit
+     * without defined transformation.
+     *
+     * @test
+     * @return void
+     */
+    public function mixed_digits_and_no_transformation_test(){
+
+        //Create an array of numbers that have no digits for transformation
+        $input_number = 3075;
+        $rand = rand(0,count($this->digits) - 1);
+
+        //Create digits that have no transformation
+        $no_transformation_digits = collect(range(0,9));
+        //Reject numbers that are already digits that have defined transformations
+        foreach ($this->digits as $digit){
+
+            //Reject the numbers that when converted to array contain the current digit
+            $no_transformation_digits = $no_transformation_digits->reject(function ($number) use ($digit) {
+                return collect(str_split((string)$number))  //Split number by digit
+                ->contains((string)$digit['digit']);    //Compare with current digit
+            });
+        }
+        $no_transformation_digits = $no_transformation_digits->toArray();
+        $no_transformation_digits = array_values($no_transformation_digits);    //Reassign array keys after removing elements
+
+        $input_number = [];
+        $expected_output = [];
+        //Insert three random digits that have defined transformation and add them to expected output
+        for ($i=0; $i<3; $i++){
+            $random_digit = $this->digits[rand(0,count($this->digits) - 1)];
+            array_push(
+                $input_number,
+                $random_digit['digit']
+            );
+            array_push(
+                $expected_output,
+                $random_digit['output']
+            );
+        }
+        //Insert random no transformation digit
+        $random_digit = $no_transformation_digits[rand(0,count($no_transformation_digits) - 1)];
+        array_push(
+            $input_number,
+            $random_digit
+        );
+        //Insert another random digit that has defined transformation
+        $random_digit = $this->digits[rand(0,count($this->digits) - 1)];
+        array_push(
+            $input_number,
+            $random_digit['digit']
+        );
+        array_push(
+            $expected_output,
+            $random_digit['output']
+        );
+
+        //Convert input number to integer
+        $input_number = (integer)collect($input_number)->join('');
+
+        $this->assertSame(
+            [
+                'success'=>true,
+                'input'=>$input_number,
+                'result'=>$expected_output
+            ],
+            $this->occurrences_service->occurrences($input_number)
+        );
+    }
+}

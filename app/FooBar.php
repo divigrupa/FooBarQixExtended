9ba92603224551f7214a243cbb8eac3471f480ef
laumags <laumadz@gmail.com>
2022-08-22 16:21:59 +0300

    step 1 wrote implementation for all tests to work

--- app/FooBar.php
+++ app/FooBar.php
@@ -2,10 +2,27 @@
 
 namespace App;
 
-class FooBar 
+class FooBar
 {
+    private int $number;
+    private const FOO = 3;
+    private const BAR = 5;
+
+    public function __construct(int $number)
+    {
+        $this->number = $number;
+    }
+
     public function run(): string
     {
-        return "FooBar result";
+        if ($this->number % self::BAR == 0 && $this->number % self::FOO == 0) {
+            return "Foo, Bar";
+        } elseif ($this->number % self::FOO == 0) {
+            return "Foo";
+        } elseif ($this->number % self::BAR == 0) {
+            return "Bar";
+        } else {
+            return (string) $this->number;
+        }
     }
 };
--- tests/FooBarTest.php
+++ tests/FooBarTest.php
@@ -4,20 +4,20 @@ use App\FooBar;
 
 test("should return 'Foo' if number is multiple of 3", function() {
     $fooBar = new FooBar(3);
-    expect($fooBar->run)->toEqual("Foo");
+    expect($fooBar->run())->toEqual("Foo");
 });
 
 test("should return 'Bar' if number is multiple of 5", function() {
     $fooBar = new FooBar(5);
-    expect($fooBar->run)->toEqual("Bar");
+    expect($fooBar->run())->toEqual("Bar");
 });
 
 test("should return 'Foo, Bar' if number is multiple of 3 and 5", function() {
     $fooBar = new FooBar(15);
-    expect($fooBar->run)->toEqual("Foo, Bar");
+    expect($fooBar->run())->toEqual("Foo, Bar");
 });
 
 test("should return given number as a string if there is no transformation to do", function() {
     $fooBar = new FooBar(7);
-    expect($fooBar->run)->toEqual("7");
+    expect($fooBar->run())->toEqual("7");
 });

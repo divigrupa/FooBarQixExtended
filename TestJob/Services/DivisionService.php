1e522ba8ca0382bc58aebf143dbc7894c33d3a79
systemaworking <69110904+systemaworking@users.noreply.github.com>
2022-08-30 08:28:47 +0300

    Step 1: Tests created
    - unit tests added
    - Services/DivisionService.php stub added

---
+++ TestJob/Services/DivisionService.php
@@ -0,0 +1,21 @@
+<?php
+namespace TestJob\Services;
+
+/*
+ * We need you to implement a service (we will let aside the Rest/web service side of it). It will take a number (positive integer) and provide:
+
+    "Foo" if this number is multiple of 3
+    "Bar" if this number is multiple of 5
+
+    If number have several multiples, they appear in natural order (Foo, Bar).
+
+    We will return the given number as a string if there is no transformation to do.
+ */
+
+class DivisionService
+{
+    public static function run( int $number ): string
+    {
+        return "";
+    }
+}
---
+++ TestJob/Tests/Services/DivisionServiceTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use TestJob\Services\DivisionService;
+
+final class DivisionServiceTest extends TestCase
+{
+    public function testPushAndPop(): void
+    {
+        # invalid numbers
+        $this->assertEquals( DivisionService::run( -1 ), "-1" );
+        $this->assertEquals( DivisionService::run( -100 ), "-100" );
+        $this->assertEquals( DivisionService::run( 0 ), "0" );
+
+        # normal division
+        $this->assertEquals( DivisionService::run( 3 ), "Foo" );
+        $this->assertEquals( DivisionService::run( 6000 ), "Foo" );
+        $this->assertEquals( DivisionService::run( 5 ), "Bar" );
+        $this->assertEquals( DivisionService::run( 5000 ), "Bar" );
+        $this->assertEquals( DivisionService::run( 15 ), "Foo, Bar" );
+        $this->assertEquals( DivisionService::run( 3000 ), "Foo, Bar" );
+
+        # fail division
+        $this->assertEquals( DivisionService::run( 19 ), "19" );
+        $this->assertEquals( DivisionService::run( 61 ), "61" );
+    }
+}

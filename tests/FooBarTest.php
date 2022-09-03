eb40721a9c80f1002114f104581712205dafb28c
Kristaps Laksa <ljaksa.kristaps@gmail.com>
2022-09-03 13:48:01 +0300

    Added not mandatory functionality for occurrences. Tests passed.

--- tests/FooBarTest.php
+++ tests/FooBarTest.php
@@ -42,13 +42,18 @@ test(
     'If the given number contains specific digit,
     we will append a word to the transformation in the order they appear in the number.'
     , function () {
-    $elements=[
+    $multiples=[
+        $foo=new Element('Foo',3),
+        $bar=new Element('Bar',5),
+        $qix=new Element('Qix',7)
+    ];
+    $occurrences=[
         $foo=new Element('Foo',3),
         $bar=new Element('Bar',5),
         $qix=new Element('Qix',7)
     ];
 
-    $function = new NumberTransformation($elements);
+    $function = new NumberTransformation($multiples,$occurrences);
     expect($function->execute(7))->toBe('Qix') ;
     expect($function->execute(105))->toBe('Foo,Bar,Qix');
     expect($function->execute(177))->toBe('Foo');
@@ -56,5 +61,4 @@ test(
     expect($function->execute(152))->toBe('Bar');
     expect($function->execute(17434))->toBe('Qix,Foo');
     expect($function->execute(153752))->toBe('Bar,Foo,Qix,Bar');
-
 });

3d73973d6eddafe9341cd75faef3e84486e15f8c
Ivans Grjaznihs <ivans.grjaznihs@hotmail.com>
2022-09-13 12:41:50 -0400

    update tests (step 2)

--- homeworkTest.php
+++ homeworkTest.php
@@ -15,7 +15,15 @@ class HomeWorkTest extends TestCase {
      * @dataProvider taskProvider
      */
     public function testTask($value, $expection, $obj) {
-        $this->assertSame($expection, $obj->task($value));
+        $this->assertSame($expection, $obj->Task($value));
+    }
+
+    /**
+     * @depends testInclude
+     */
+    public function testNegativeIntegerException($obj) {
+        $this->expectException(InvalidArgumentException::class);
+        $obj->Task(-1);
     }
 
     public function taskProvider()
@@ -23,7 +31,11 @@ class HomeWorkTest extends TestCase {
         return [
             'number is multiple of 3' => [3, 'Foo'],
             'number is multiple of 5' => [5, 'Bar'],
-            'number has several multiples' => [15, 'Foo, Bar'],
+            'number is multiple of 7' => [7, 'Qix'],
+            'number is multiple of 3 and 5' => [15, 'Foo, Bar'],
+            'number is multiple of 3 and 7' => [21, 'Foo, Qix'],
+            'number is multiple of 5 and 7' => [35, 'Bar, Qix'],
+            'number is multiple of 3, 5 and 7' => [105, 'Foo, Bar, Qix'],
             'number has not multiples' => [11, '11']
         ];
     }

36af20dec49a854a66c099d565d1ef8cc547f4dc
Myra-tech <ance.l.tetere@gmail.com>
2022-09-28 12:16:59 +0300

    Step 1: Initial fail

---
+++ src/test/java/FooBarQixTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FooBarQixTest {
+
+    @Test
+    public void initialFooBarFailTest() throws Exception {
+        Assert.fail("Not yet implemented");
+    }
+}

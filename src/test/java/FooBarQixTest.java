e11d0eb3064b2246d85d9be9b07fca75b22fc89d
Myra-tech <ance.l.tetere@gmail.com>
2022-09-29 03:15:40 +0300

    Step 1: Watching the tests fail.

--- src/test/java/FooBarQixTest.java
+++ src/test/java/FooBarQixTest.java
@@ -14,7 +14,7 @@ public class FooBarQixTest {
         String expectedName = "Foo";
         String expectedNumber = String.valueOf(theNumber);
 
-        String resultName = ascription.ascribeNondescriptiveIdentifierToTheNumber(theNumber);
+        String resultName = ascription.ascribeFooToTheNumber(theNumber);
         String resultNumber = ascription.tranformTheNumber(theNumber);
 
         assertEquals(expectedName, resultName);
@@ -29,7 +29,7 @@ public class FooBarQixTest {
         String expectedName = "Bar";
         String expectedNumber = String.valueOf(theNumber);
 
-        String resultName = ascription.ascribeNondescriptiveIdentifierToTheNumber(theNumber);
+        String resultName = ascription.ascribeBarToTheNumber(theNumber);
         String resultNumber = ascription.tranformTheNumber(theNumber);
 
         assertEquals(expectedName, resultName);
@@ -44,7 +44,7 @@ public class FooBarQixTest {
         String expectedName = "Foo" + "Bar";
         String expectedNumber = String.valueOf(theNumber);
 
-        String resultName = ascription.ascribeNondescriptiveIdentifierToTheNumber(theNumber);
+        String resultName = ascription.ascribeFooBarToTheNumber(theNumber);
         String resultNumber = ascription.tranformTheNumber(theNumber);
 
         assertEquals(expectedName, resultName);

a39f748181ad1aa1e2d7037a2d22f2bf9d69f648
Myra-tech <ance.l.tetere@gmail.com>
2022-09-29 03:02:21 +0300

    Step 1: Watching the tests fail.

--- src/test/java/FooBarQixTest.java
+++ src/test/java/FooBarQixTest.java
@@ -1,10 +1,53 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FooBarQixTest {
 
+    Ascription ascription = new Ascription();
+
     @Test
-    public void initialFooBarFailTest() throws Exception {
-        Assert.fail("Not yet implemented");
+    public void shouldAscribeFooCorrectlyTest() throws Exception {
+
+        int theNumber = 18;
+        String expectedName = "Foo";
+        String expectedNumber = String.valueOf(theNumber);
+
+        String resultName = ascription.ascribeNondescriptiveIdentifierToTheNumber(theNumber);
+        String resultNumber = ascription.tranformTheNumber(theNumber);
+
+        assertEquals(expectedName, resultName);
+        assertEquals(expectedNumber, resultNumber);
+    }
+
+
+    @Test
+    public void shouldAscribeBarCorrectlyTest() throws Exception {
+
+        int theNumber = 20;
+        String expectedName = "Bar";
+        String expectedNumber = String.valueOf(theNumber);
+
+        String resultName = ascription.ascribeNondescriptiveIdentifierToTheNumber(theNumber);
+        String resultNumber = ascription.tranformTheNumber(theNumber);
+
+        assertEquals(expectedName, resultName);
+        assertEquals(expectedNumber, resultNumber);
+    }
+
+
+    @Test
+    public void shouldAscribeFooBarCorrectlyTest() throws Exception {
+
+        int theNumber = 15;
+        String expectedName = "Foo" + "Bar";
+        String expectedNumber = String.valueOf(theNumber);
+
+        String resultName = ascription.ascribeNondescriptiveIdentifierToTheNumber(theNumber);
+        String resultNumber = ascription.tranformTheNumber(theNumber);
+
+        assertEquals(expectedName, resultName);
+        assertEquals(expectedNumber, resultNumber);
     }
 }
